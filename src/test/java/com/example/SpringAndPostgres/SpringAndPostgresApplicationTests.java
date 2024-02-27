471e016a664aaeaccc504b004da9dbd8d7f929e7
DiegoAlejandroRJ <diego6382@gmail.com>
2024-02-27 13:12:26 -0500

    spring and postgres

---
+++ src/test/java/com/example/SpringAndPostgres/SpringAndPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAndPostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
